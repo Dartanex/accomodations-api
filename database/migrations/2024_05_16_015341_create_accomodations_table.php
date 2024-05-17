ddf4bdbe76d48c681de038f0cf2248ff0aa53cd9
Josue Orantes <daniel02josue@gmail.com>
2024-05-16 19:13:10 -0600

    :sparkles:feat(database):add migration accomodations database and models

---
+++ database/migrations/2024_05_16_015341_create_accomodations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('accomodations', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->string('name');
+            $table->string('address');
+            $table->integer('capacity');
+            $table->integer('rooms');
+            $table->string('image_url');
+            $table->decimal('price');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('accomodations');
+    }
+};
