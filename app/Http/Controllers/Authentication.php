49e7b622dbe87db269ca965f864dfba6a5310fde
Josue Orantes <daniel02josue@gmail.com>
2024-05-27 17:30:03 -0600

    :sparkles:feat(authentication):add authentication controller

--- app/Http/Controllers/Authentication.php
+++ app/Http/Controllers/Authentication.php
@@ -2,11 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Authentication as ModelsAuthentication;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class Authentication extends Controller
 {
     public function register(Request $request){
+
+        $validator = Validator::make($request->all, [
+            'fullname' => 'required|string|max:100',
+            'username' => 'required|string|max:30',
+            'email' => 'required|email',
+            'password' => 'required|string|min:8|max:24',
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                'status' => false,
+                'message' => 'Validator Error!',
+                'data' => $validator->errors()
+            ], 409);
+        }
+
+        $user = new ModelsAuthentication();
+        $user->fullname = $request->fullname;
+        $user->username = $request->username;
+        $user->email = $request->email;
+        $user->password = $request->password;
+        $user->role = 'mortal';
+        $user->save();
 
         return response()->json([
             'message' => 'Register working',
